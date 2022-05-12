87400aeec52933f41339fb44f0882ca8ef20a18c
Samuel Seve <samuelseve1@gmail.com>
2022-05-12 10:38:54 -0300

    att

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
-
-import { ProductList } from './styles';
-import { api } from '../../services/api';
-import { formatPrice } from '../../util/format';
-import { useCart } from '../../hooks/useCart';
+import React, { useState, useEffect } from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
+import { ProductList } from './styles'
+import { api } from '../../services/api'
+import { formatPrice } from '../../util/format'
+import { useCart } from '../../hooks/useCart'
+import axios from 'axios'
+import { isExternalModule } from 'typescript'
 
 interface Product {
-  id: number;
-  title: string;
-  price: number;
-  image: string;
+  id: number
+  title: string
+  price: number
+  image: string
 }
 
 interface ProductFormatted extends Product {
-  priceFormatted: string;
+  priceFormatted: string
 }
 
 interface CartItemsAmount {
-  [key: number]: number;
+  [key: number]: number
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const [products, setProducts] = useState<ProductFormatted[]>([])
   // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
@@ -31,11 +32,12 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      await api.get('/products').then(res => {
+        setProducts(res.data)
+      })
     }
-
-    loadProducts();
-  }, []);
+    loadProducts()
+  }, [])
 
   function handleAddProduct(id: number) {
     // TODO
@@ -43,25 +45,29 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map((item, index) => {
+        return (
+          <li key={index}>
+            <img src={item.image} alt={item.title} />
+            <strong>{item.title}</strong>
+            <span>{item.price}</span>
+            <button
+              type="button"
+              data-testid="add-product-button"
+              // onClick={() => handleAddProduct(product.id)}
+            >
+              <div data-testid="cart-product-quantity">
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {/* {cartItemsAmount[product.id] || 0} */} 
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        )
+      })}
     </ProductList>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
